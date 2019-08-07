969250ac7f0fb0040867bb3ff30d69409543191d
Mari <mariescuela@outlook.com>
2019-08-06 22:20:38 -0300

    Act

--- pruebalista.py
+++ pruebalista.py
@@ -2,6 +2,9 @@ from tdalista import cargaAuto, insertar, eliminar, barrido
 from tdalista import lista_vacia, tamanio, busqueda, Lista
 from tdalista import cargaString
 l1 = Lista()
+l2 = Lista()
+l3 = Lista()
+
 
 # ej1
 '''cargaAuto(l1, 10)
@@ -9,15 +12,21 @@ barrido(l1)
 print('El tamanio es: ' + str(l1.tamanio))'''
 
 # ej2
-cargaString(l1, 10)
+'''cargaString(l1, 10)
 barrido(l1)
 aux1 = l1.inicio
 aux = l1.inicio
 while aux is not None:
-    if ((aux == 'a') or (aux == 'e') or (aux == 'i') or (aux == 'o')
-    or (aux == 'u') or (aux == 'A') or (aux == 'E') or (aux == 'I')
-    or (aux == 'O') or (aux == 'U')):
-        eliminar(l1, aux)
-        aux = l1.inicio
-print('aca')
+    if ((aux.info == 'a') or (aux.info == 'e') or (aux.info == 'i') or
+    (aux.info == 'o') or (aux.info == 'u') or (aux.info == 'A') or
+    (aux.info == 'E') or (aux.info == 'I') or (aux.info == 'O') or
+    (aux.info == 'U')):
+        eliminar(l1, aux.info)
+    aux = aux.sig
+print('lista sin vocales')
+barrido(l1)'''
+
+
+# ej3
+cargaAuto(l1)
 barrido(l1)
