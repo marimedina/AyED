8148388786246794e793efe5d01602a283bf18d4
Mari <mariescuela@outlook.com>
2019-09-03 20:35:14 -0300

    Actualizado

--- pruebaarbol.py
+++ pruebaarbol.py
@@ -1,11 +1,31 @@
 from tdaarbol import NodoArbol, busqueda, eliminar, inorden
 from tdaarbol import postorden, preorden, insertar, reemplazar
+import random
 
-r = None
+'''r = None
 r = insertar(r, 4)
 r = insertar(r, 6)
 r = insertar(r, 3)
 r = insertar(r, 10)
 inorden(r)
 print('aca')
+preorden(r)'''
+
+# ej1
+r = None
+for i in range(0, 10):
+    r = insertar(r, random.randint(0, 100))
+# Parte A
+print('Barrido preorden:')
 preorden(r)
+print('Barrido inorden:')
+inorden(r)
+print('Barrido postorden:')
+postorden(r)
+# Parte B
+n = 45
+if busqueda(r, n) is None:
+    print('El numero no se encuentra en el arbol')
+else:
+    print('EL numero se encuentra en el arbol')
+# Parte C
