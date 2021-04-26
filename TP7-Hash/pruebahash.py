5dd927a8e63f7e8ad0aad7737e4efbe58bc98b7b
marimedina <mariescuela@outlook.com>
2021-04-26 16:45:49 -0300

    Ej8 arreglado

--- TP7-Hash/pruebahash.py
+++ TP7-Hash/pruebahash.py
@@ -174,59 +174,385 @@ barrido_cerrada(t)
 '''
 
 
-#Ej 6 -
+#Ej 6 - Darth Vader le encarga desarrollar los algoritmos para organizar los Troopers cumpliendo
+# con ciertas demandas:
 '''
 legiones = ['FL', 'TF', 'TK', 'CT', 'FN', 'FO']
-legion = ''
-num = ''
-def codigo():
+tdigitos = crear_tabla_abierta(2000)
+tlegion = crear_tabla_abierta(2000)
+
+# Parte A y B - Generar 2000 Troopers siguiendo el formato contemplando las
+# siguientes legiones FL, TF, TK, CT, FN, FO y los digitos generados
+#aleatoriamente;
+
+def hashTrooper(clave):
+    ind = 0
+    if len(clave) == 2:
+        ind = (ord(clave[0])*3) + (ord(clave[1])*7)
+    else:
+        ind = (ord(clave[0])* ord(clave[1])) + ord(clave[2])
+
+    return ind
+
+def busqT(tabla, digitos):
+    ind = hashTrooper(digitos)
+    ind = ind % len(tabla)
+
+    return tabla[ind]
+
+def nuevoT():
     legion = random.choice(legiones)
-    num = random.randint(0000, 9999)
-    codigo = legion + '-' + num
-    return codigo
+    cohoerte = str(random.randint(0,9))
+    siglo = str(random.randint(0,9))
+    escuadra = str(random.randint(0,9))
+    numero = str(random.randint(0,9))
+    trooper = legion + '-' + cohoerte + siglo + escuadra + numero
 
-def insertar_codigo(t, codigo):
-    legion, num = codigo.split('-')
-    indice = hash(legion)
-    indice = indice % len(t)
-    insertar(t[indice], codigo)
+    return trooper
 
-def hash(codigo):
-    legion, num = codigo.split("-")
-    return (legion + num)
+def trooperLegion(tabla, trooper):
+    legion, codigo = trooper.split('-')
+    indice = hashTrooper(legion)
+    indice = indice % len(tabla)
+    insertar(tabla[indice], trooper)
 
-t = crear_tabla_abierta(10)
-for i in range(0, 10):
-    insertar_codigo(t, codigo)
-barrido_abierta(t)
+def trooperDig(tabla, trooper):
+    legion, codigo = trooper.split('-')
+    indice = hashTrooper(codigo[1:len(codigo)])
+    indice = indice % len(tabla)
+    insertar(tabla[indice], trooper)
+
+def agregarT(trooper):
+    trooperLegion(tlegion, trooper)
+    trooperDig(tdigitos, trooper)
+
+
+
+
+for i in range(0, 20):
+    agregarT(nuevoT())
+
+barrido_abierta(tlegion)
+barrido_abierta(tdigitos)
+print('')
+'''
+# Parte C - Ahora obtenga todos los Stroormtropers terminados en 781 para asignarlos a una
+# mision de asalto y a los terminados en 537 para una mision de exploracion.
+'''
+asalto = []
+buscado = busqT(tdigitos,'781')
+if buscado > 0:
+    aux = buscado.inicio
+    while aux is not None:
+        if aux.info[4:] == '781':
+            asalto.append(aux.info)
+        aux = aux.sig
+
+print('Troopers para mision asalto:')
+print(asalto)
+
+exploracion = []
+buscado = busqT(tdigitos, '537')
+if buscado > 0:
+    aux = buscado.inicio
+    while aux is not None:
+        if aux.info[4:] == '537':
+            exploracion.append(aux.info)
+        aux = aux.sig
+print('Troopers para mision de exploracion:')
+print(exploracion)
+'''
+
+# Parte D - Ahora obtenga los Strormtroopers de la legion CT para que custodien a Darth
+#Vader a una mision de exploracion al planeta Hoth y los de la legion TF para una
+#mision de exterminacion a Endor.
+'''
+exploracion = []
+buscado = busqT(tlegion, "CT")
+if buscado >= 0:
+    print(aca)
+    aux = buscado.inicio
+    while aux is not None:
+        if aux.info[:2] == "CT":
+            exploracion.append(aux.info)
+        aux = aux.sig
+print('Troopers para custodia de Darth Vader:')
+print(exploracion)
+
+exterminacion = []
+busc = busqT(tlegion, "TF")
+if busc >= 0:
+    aux = busc.inicio
+    while aux is not None:
+        if aux.info[0:2] == "TF":
+            exploracion.append(aux.info)
+        aux = aux.sig
+print('Troopers para misison de exterminacion a Endor:')
+print(exterminacion)
 '''
 
 
-#Ej 7 -
+
+#Ej 7 - ERROR EN HASH
+
+'''
 t_tipo = crear_tabla_cerrada(30)
-t_pokemon = crear_tabla_abierta(15)
 tipos = ['fuego', 'agua', 'planta', 'normal', 'electrico', 'hielo',
         'lucha', 'veneno', 'tierra', 'volador']
 
-
-
-
-
-def insertar_pokemon(tabla, pokemon):
-    indice = hash(pokemon)
-    indice = indice % len(t)
-    if tabla[indice] is not None:
-        indice = rehash(tabla, indice)
-    tabla[indice] = pokemon
-
-    return tabla
-
-for i in range(0,10):
-    numero = str([i])
-    nombre = 'Nombre ' + str([i])
+def nuevoP(numero=0):
+    num = numero
+    nombre = 'Nombre' + str(num)
+    c_tipo = random.randint(1,2)
     tipo = random.choice(tipos)
-    nivel = random.randint(1,100)
-    pokemon = [numero, nombre, tipo, nivel]
-    insertar_pokemon(t_tipo, pokemon)
+    if c_tipo == 2:
+        tipo += '/' + random.choice(tipos)
+    nivel = random.randint(0,100)
 
-barrido_cerrada(t_tipo)
+    return [num, nombre, tipo, nivel]
+
+def hashP(clave):
+    if type(clave) is int:
+        clave = str(clave)
+    indice = 0
+    for elemento in clave:
+        indice += ord(elemento)
+    return indice
+
+def sondeoTipo(tabla, tipo, indice):
+    while (tabla[indice] is not None) and (tabla[indice][0] != tipo):
+        indice += 1
+        if indice == len(tabla):
+            indice = 0
+    return indice
+
+def insertarNum(tabla, pokemon):
+    indice = hashP(pokemon) % len(tabla)
+    campos(tabla[indice], pokemon, 0)
+
+def insertarEnTipo(t_tipo, pokemon):
+    tip = pokemon[2].split('/')
+
+    for tipo in tip:
+        indice = hashP(tipo) % len(t_tipo)
+        if t_tipo[indice] is None:
+            t_tipo[indice] = [tipo, crear_tabla_abierta(15)]
+            insertarNum(t_tipo[indice][1], pokemon)
+        else:
+            if t_tipo[indice][0] == tipo:
+                insertarNum(t_tipo[indice][1], pokemon)
+            else:
+                indice = sondeoTipo(t_tipos, tipo, indice)
+                if t_tipo[indice] is not None:
+                    t_tipo[indice] = [tipo, crear_tabla_abierta(15)]
+                insertarNum(t_tipo[indice][1], pokemon)
+
+for i in range(15):
+    pokemon = nuevoP(i)
+    print(pokemon)
+    insertarEnTipo(t_tipo, pokemon)
+'''
+
+# Ej 8 - la princesa Leia nos encarga el desarrollo de un algoritmo
+# de encriptacion para las comunicaciones rebeldes.
+'''
+def encriptar(texto):
+    clave = ''
+    for letra in texto:
+        p1 = str(ord(letra)*37)
+        p2 = hex(ord(letra)*2)
+        clave += p1[0] + p2[1] + p2[3] + p1[1] + p1[2] + p2[0] + p1[3] + p2[2]
+    return clave
+
+
+text = 'Ayudame, Obi-Wan Kenobi. Tu eres mi unica esperanza'
+texto_encriptado = encriptar(text)
+print('Mensaje encriptado:')
+print(texto_encriptado)
+
+def desencriptar(clave):
+    texto = ''
+    while len(clave) > 0:
+        caracter = ''
+        caracter += clave[0] + clave[3] + clave[4] + clave[6]
+        clave = clave[8:]
+        caracter = int(caracter)
+        caracter = int(caracter/37)
+        caracter = chr(caracter)
+        texto += caracter
+    return texto
+
+print('')
+print('Mensaje desencriptado:')
+text1 = desencriptar(texto_encriptado)
+print(text1)
+'''
+
+
+# Ej 9 - Desarrollar un algoritmo que permita cifrar
+# y descifrar un mensaje caracter a caracter
+'''
+t_valoresCodif = crear_tabla_cerrada(10)
+t_valoresCodif[1] = 'abc'
+t_valoresCodif[2] = 'def'
+t_valoresCodif[3] = 'ghi'
+t_valoresCodif[4] = 'jkl'
+t_valoresCodif[5] = 'mnn'
+t_valoresCodif[6] = 'opq'
+t_valoresCodif[7] = 'rst'
+t_valoresCodif[8] = 'uvw'
+t_valoresCodif[9] = 'xyz'
+t_valoresCodif[0] = '#?&'
+
+def hashC(caracter):
+    cad = ''
+    ascii = ord(caracter)
+    ascii = str(ascii)
+    for dig in ascii:
+        digito = int(dig)
+        clave = t_valoresCodif[digito]
+        cad += clave
+    cad += '%'
+
+    return cad
+
+def encriptar(texto):
+    cadena = ''
+    for letra in texto:
+        cadena += hashC(letra)
+
+    return cadena
+
+texto = 'Ayudame, Obi Wan Kenobi. Tu eres mi unica esperanza'
+print('Texto encriptado:')
+text_e = encriptar(texto)
+print(text_e)
+
+def desencriptar(texto):
+    msj = ''
+    texto_div = texto.split('%')
+    for elemento in texto_div:
+        if len(elemento) > 0:
+            digitos = ''
+            for i in range(0, len(elemento), 3):
+                if elemento[i] == "#":
+                    digitos += "0"
+                elif elemento[i] == "a":
+                    digitos += "1"
+                elif elemento[i] == "d":
+                    digitos += "2"
+                elif elemento[i] == "g":
+                    digitos += "3"
+                elif elemento[i] == "j":
+                    digitos += "4"
+                elif elemento[i] == "m":
+                    digitos += "5"
+                elif elemento[i] == "o":
+                    digitos += "6"
+                elif elemento[i] == "r":
+                    digitos += "7"
+                elif elemento[i] == "u":
+                    digitos += "8"
+                elif elemento[i] == "x":
+                    digitos += "9"
+            digitos = int(digitos)
+            msj += chr(digitos)
+
+    return msj
+print('')
+print('Texto desencriptado:')
+text_d = desencriptar(text_e)
+print(text_d)
+'''
+
+# Ej 10 - Nick Fury director de la agencia S.H.I.E.L.D. intenta detener a la organizacion Hydra y a su
+# lider Red Skull, los agentes de la agencia pueden interceptar los mensajes de Hydra pero
+# estan cifrados por los cual no pueden hacer nada con estos, por suerte el Capitan America
+# en una mision encubierta logro determinar las pautas del metodo de codificacion. Ahora
+# Fury nos solicita desarrollar el algoritmo que permita decodificar los mensajes
+'''
+def calculoComplemento(caracter):
+    if ord(caracter) <= 78:
+        return 79 + ord(caracter) - 32
+    else:
+        return 32 + ord(caracter) - 79
+
+
+def hashS(string):
+    hash = 5381
+    for caracter in string:
+        hash = ((hash << 5) + hash) + ord(caracter)
+    return hash & 0xFFFFFFFF
+
+def charAChar(seg):
+    n4_dig = ''
+    primer_caracter = seg[:4]
+    ultimo_caracter = seg[4]
+    complemento = ord(ultimo_caracter)
+    for elemento in primer_caracter:
+        elemento = ord(elemento) - complemento
+        elemento = int(sqrt(elemento))
+        elemento = str(elemento)
+        n4_dig += elemento
+    n4_dig = int(n4_dig)
+    chr_ascii = int(n4_dig/37)
+    caracter = chr(chr_ascii)
+    return caracter
+
+def charA5Char(caracter):
+    caracteres = ''
+    char_ascii = ord(caracter)
+    char_ascii *= 37
+    complemento = calculoComplemento(caracter)
+    char_ascii = str(char_ascii)
+    for digito in char_ascii:
+        digito = int(digito)
+        num = pow(digito, 2) + complemento
+        caracter = chr(int(num))
+        caracteres += caracter
+    caracteres += chr(complemento)
+
+    return caracteres
+
+def decodificar(mensaje):
+    texto = ""
+    i = 0
+    while i < len(mensaje):
+        segmento = mensaje[i:i+5]
+        indice = hashS(segmento) % len(tabla)
+        if (tabla[indice] is None):
+            caracter = charAChar(segmento)
+            tabla[indice] = caracter
+        else:
+            caracter = tabla[indice]
+        texto += caracter
+        i += 5
+    return texto
+
+t = crear_tabla_cerrada(227)
+
+#Abro y leo los msj
+archivo = open('msj1.txt', encoding='utf-8')
+msj_codif = archivo.read()
+archivo.close()
+
+archivo2 = open('msj2.txt', encoding='utf-8')
+msj2_codif = archivo2.read()
+archivo2.close()
+
+archivo3 = open('msj3.txt', encoding='utf-8')
+msj3_codif = archivo3.read()
+archivo3.close()
+
+#decodifico los 3
+msj_decodif = decodificar(msj_codif)
+msj2_decodif = decodificar(msj2_codif)
+msj3_decodif = decodificar(msj3_codif)
+
+#Muestro
+print('Mensaje 1:', msj_codif)
+print('')
+print('Mensaje 2:', msj2_codif)
+print('')
+print('Mensaje 3:', msj3_codif)
+'''
